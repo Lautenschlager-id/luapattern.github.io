1bc15d76ef928498303a18235c75ee384f7d07d8
Lautenschlager <tai.romani@hotmail.com>
2018-11-06 19:32:37 -0200

    Release

---
+++ sys.js
@@ -0,0 +1,69 @@
+function normalizeQuotes(t) {
+	return t.replace("\"", "\\\"").replace("'", "\\'");
+}
+
+function contentChanged(){
+	var pattern = document.getElementById("pattern");
+	var content = document.getElementById("content");
+	var match = document.getElementById("matched");
+	var gmatch = document.getElementById("gmatched");
+	var counter = document.getElementById("counter");
+
+	try
+	{
+		var lua = fengari.load(`
+local str = "` + normalizeQuotes(content.value) + `"
+local pattern = "` + normalizeQuotes(pattern.value) + `"
+
+local matches, counter = { }, 0
+
+local i, e = 1, 0
+local last_i, last_e = 0, 0
+
+local len, f = #str
+
+while true do
+    counter = counter + 1
+    f = len - (len - last_e)
+    i, e = string.find(string.sub(str, f + 1), pattern)
+    if not i or not e or e == last_e then
+        break
+    end
+
+    last_i, last_e = f + i, f + e
+    matches[counter] = { last_i, last_e, (string.sub(str, last_i, last_e)) }
+end
+
+return { len = counter - 1, matches = matches }
+`)();
+		let obj, len = lua.get("len");
+		if (len == 0)
+			throw null;
+
+		gmatch.innerHTML = "";
+		for (let index = 1; index <= len; index++)
+		{
+			obj = lua.get("matches").get(index);
+			obj = "<i>[" + obj.get(1) + ":" + obj.get(2) + "]</i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + obj.get(3) + "<br>";
+
+			if (index == 1)
+				match.innerHTML = obj;
+			gmatch.innerHTML += obj;
+		}
+		counter.innerHTML = "*" + len + " matches found";
+	}
+	catch(_)
+	{
+		counter.innerHTML = match.innerHTML = gmatch.innerHTML = "";
+	}
+}
+
+function autosize(textarea){
+	textarea.style.height = "auto";
+	
+	var height = textarea.scrollHeight - 5;
+	if (height > 300)
+		textarea.style.overflow = "auto";
+
+	textarea.style.height = Math.min(300, height) + "px";
+}
